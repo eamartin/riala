8766712a5b6bfbfc865520b6d702c8f5fbe078c1
Eric Martin <e.a.martin1337@gmail.com>
2011-01-17 16:13:42 +0000

    Basic tests passing.

--- closet/__init__.py
+++ closet/__init__.py
@@ -27,31 +27,31 @@ class RiakModelList(object):
     @property
     def results(self):
         if not hasattr(self, '_cache'):
-            self._cache = [self.result_cls(k, **v) for k,v in self.query.run()]
+            self._cache = [self.result_cls(_key=k, **v) for k,v in self.query.run()]
         return self._cache
 
 
 class RiakModel(micromodels.Model):
     '''Closets are a place to store your messy data.'''
 
-    def __init__(self, key=None, _lazy=None, **kwargs):
-        self.key = key
+    def __init__(self, _key=None, _lazy=None, **kwargs):
         self._lazy = _lazy
+        self._key = _key
         if not self._lazy:
             super(RiakModel, self).__init__(kwargs)
 
     def __getattr__(self, name):
-        if self._lazy:
-            self.__init__(self.key, **self._lazy.get_data())
+        if super(RiakModel, self)._lazy:
+            self.__init__(_key=self.key, **self._lazy.get_data())
         return super(RiakModel, self).__getattr__(name)
 
     def set_key(self, key):
-        self.key = key
+        self._key = key
         return self
 
     @classmethod
     def get(cls, key):
-        return cls(key, lazy=cls.bucket.get(key))
+        return cls(_key=key, lazy=cls.bucket.get(key))
 
     @classmethod
     def search(cls, query):
@@ -64,4 +64,4 @@ class RiakModel(micromodels.Model):
         return RiakModelList(cls, query_object)
 
     def store(self):
-        self.bucket.new(self.key, self.to_dict(serial=True)).store()
+        self.bucket.new(self._key, self.to_dict(serial=True)).store()
