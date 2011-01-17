38b05bbf0571d9a3ffe22a91df8ca5a7b2751627
Eric Martin <e.a.martin1337@gmail.com>
2011-01-17 22:10:38 +0000

    Laziness and tests working.

--- closet/__init__.py
+++ closet/__init__.py
@@ -11,8 +11,9 @@ import micromodels
 class RiakConnection(riak.RiakClient):
 
     def register_model(self, model):
-        bucket_name = getattr(model, 'bucket') or model.__name__.lower()
+        bucket_name = getattr(model, 'bucket', False) or model.__name__.lower()
         model.bucket = self.bucket(bucket_name)
+        return model
 
 
 class RiakModelList(object):
@@ -22,7 +23,8 @@ class RiakModelList(object):
         self.result_cls = result_cls
 
         for key in ('__getindex__', '__bool__', '__len__'):
-            setattr(self, key, lambda self, *args: getattr(s.results, key)(*args))
+            setattr(self, key,
+                    lambda self, *args: getattr(s.results, key)(*args))
 
     @property
     def results(self):
@@ -35,15 +37,20 @@ class RiakModel(micromodels.Model):
     '''Closets are a place to store your messy data.'''
 
     def __init__(self, _key=None, _lazy=None, **kwargs):
+        super(RiakModel, self).__init__()
         self._lazy = _lazy
         self._key = _key
         if not self._lazy:
-            super(RiakModel, self).__init__(kwargs)
+            self.set_data(kwargs)
 
     def __getattr__(self, name):
-        if super(RiakModel, self)._lazy:
-            self.__init__(_key=self.key, **self._lazy.get_data())
-        return super(RiakModel, self).__getattr__(name)
+        if self._lazy:
+            self.__init__(_key=self._key, **self._lazy.get_data())
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            raise AttributeError("'%s' object has no attribute '%s'" %
+                                 (type(self).__name__, name))
 
     def set_key(self, key):
         self._key = key
@@ -51,7 +58,7 @@ class RiakModel(micromodels.Model):
 
     @classmethod
     def get(cls, key):
-        return cls(_key=key, lazy=cls.bucket.get(key))
+        return cls(_key=key, _lazy=cls.bucket.get(key))
 
     @classmethod
     def search(cls, query):
@@ -64,4 +71,6 @@ class RiakModel(micromodels.Model):
         return RiakModelList(cls, query_object)
 
     def store(self):
-        self.bucket.new(self._key, self.to_dict(serial=True)).store()
+        if not self._key:
+            raise Exception("'User' object must have key to be stored")
+        self.__class__.bucket.new(self._key, self.to_dict(serial=True)).store()
--- tests.py
+++ tests.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from attest import Tests, Assert
-from closet import RiakModel
+from closet import RiakConnection, RiakModel
 from micromodels import *
 
-closet = Tests()
+no_connection = Tests()
+connection_required = Tests()
 
-@closet.context
-def context():
-    class User(RiakModel):
-        name = CharField()
-        age = IntegerField()
-    yield User, dict(name='Eric Martin', age=18)
+class User(RiakModel):
+    name = CharField()
+    age = IntegerField()
 
+data = dict(name='Eric Martin', age=18)
 
-@closet.test
-def full_init(User, data):
+@no_connection.test
+def full_init():
     instance = User(**data)
     Assert(instance.to_dict()) == data
 
-@closet.test
-def late_init(User, data):
+@no_connection.test
+def late_init():
     instance = User()
     Assert(instance.to_dict()) == {}
     instance.name = data['name']
@@ -28,5 +27,22 @@ def late_init(User, data):
     instance.age = data['age']
     Assert(instance.to_dict()) == data
 
+@connection_required.context
+def make_context():
+    RiakConnection(port=8091).register_model(User)
+    key = 'eric'
+    User(_key=key, **data).store()
+    yield User, key
+
+@connection_required.test
+def test_get(User, key):
+    instance = User.get(key)
+    Assert(instance._lazy).is_not(None)
+    Assert(instance.to_dict()) == data
+    Assert(instance.name) == data['name']
+    Assert(instance._lazy).is_(None)
+
+tests = Tests([no_connection, connection_required])
+
 if __name__ == '__main__':
-    closet.main()
+    tests.main()
